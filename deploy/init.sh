c467bd27a423da36aa361083b2df2e47826be1e1
banrieen <banrieen@163.com>
2025-06-23 00:00:19 +0800

    local test multi LLM

--- deploy/init.sh
+++ deploy/init.sh
@@ -13,28 +13,39 @@ sanic server:app --host=0.0.0.0 --port=1337 --fast
 # If you still require access logs, but want to enjoy this performance boost, consider using Nginx as a proxy, and letting that handle your access logging. It will be much faster than anything Python can handle.
 # sanic path.to.server:app --no-access-logs
 
-# Python 环节管理
+# Python 环境管理
 uv venv
 source .env/bin/activate
 uv add opencv-python
 
 kill -9 `isof -ti:1337`
 
-## Start mlflow server
+## Mlflow server 
 uv add mlflow
-mlflow ui -h 10.0.56.113 -p 5000
+mlflow server --host 127.0.0.1 --port 8083
 
+## use podman start the postgres DB on host system windows11
+cd workspace
+podman volume create postgres_data
+podman run -d \
+  --name postgresql \
+  -e POSTGRES_USER=banrieen \
+  -e POSTGRES_PASSWORD=banrieen@2025 \
+  -e POSTGRES_DB=agentDB \
+  -p 5432:5432 \
+  -v \
+  
+、、:/var/lib/postgresql/data \
+  --restart=unless-stopped \
+  docker.1ms.run/library/postgres:latest
 
-# Install gradio, ffor cv test and demo
-uv add gradio
 
 ## Install and start Ray on local
+# uv add ray[default]
+# ray 
 
-uv add ray[default]
-ray 
-
-## 安装或更新 ollama 
-curl -fsSL https://ollama.com/install.sh | sh
+# ## 安装或更新 ollama 
+# curl -fsSL https://ollama.com/install.sh | sh
 
 ## autoagent studio
 uv add autogen-agentchat
@@ -50,9 +61,20 @@ ALTER USER autogen WITH PASSWORD 'autogen';
 # GRANT CREATE ON SCHEMA public TO thomas;
 
 ### Start dev service
-HOST=10.0.56.113
+HOST=192.168.124.17
 PORT=8081
 DatabaseName=autogenstudio
-Workspace="~/workspace/autogenstudio"
+Workspace="~/workspace/.autogenstudio"
 DatabaseURL=postgresql+psycopg://autogen:autogen@${HOST}/${DatabaseName}
 autogenstudio ui --appdir ${Workspace} --host ${HOST} --port ${PORT} --database-uri ${DatabaseURL}  &
+## autogenstudio ui --appdir ${Workspace} --host ${HOST} --port ${PORT}
+
+## vllm
+# uv pip install vllm
+
+# ## 使用 huggingface 国内镜像下载模型
+# uv add huggingface_hub
+# $env:HF_ENDPOINT = "https://hf-mirror.com"
+# huggingface-cli download --resume-download "meta-llama/Llama-4-Scout-17B-16E-Instruct" --local-dir llama4
+
+# 本地 podman 使用文档 file:///C:/Program%20Files/RedHat/Podman/podman-for-windows.html
